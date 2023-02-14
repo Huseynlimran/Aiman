56dd157aefce1d1101189f71a054e2b3d76c42ff
Huseynlimran <huseynlimran@gmail.com>
2023-02-14 21:16:12 +0400

    data

--- src/components/tech.js
+++ src/components/tech.js
@@ -18,9 +18,7 @@ const Tech = () => {
         <div className="tech_nav">
           <ul>
             <li>
-              <NavLink to="/rilcuyy" >
-                RILLCUY
-              </NavLink>
+              <NavLink to="/rilcuyy">RILLCUY</NavLink>
             </li>
             <li>
               <NavLink to="/trukahh">TRUKAHH</NavLink>
@@ -37,43 +35,11 @@ const Tech = () => {
           </ul>
         </div>
         <Routes>
-          <Route path="/Rilcuyy" element={<Rilcuyy />} />
           <Route path="/" element={<Rilcuyy />} />
+          <Route path="/Rilcuyy" element={<Rilcuyy />} />
           <Route path="/Trukahh" element={<Trukahh />} />
           <Route path="/Aselii" element={<Aselii />} />
         </Routes>
-        {/* <div className="tech_info">
-          <div className="tech_text">
-            <h1>
-              RILLCUY <span> AI.</span>
-            </h1>
-            <p>
-              Annotate large volumes of 3D sensor, image, and video data at high
-              throughput. ML-powered pre-labeling and an automated quality
-              assurance system ensure high quality annotations.
-            </p>
-            <ul>
-              <li>
-                <img src="img/accept.png" alt="" />
-                Manage Your Datasets
-              </li>
-              <li>
-                <img src="img/accept.png" alt="" />
-                Automate Document Processing
-              </li>
-              <li>
-                <img src="img/accept.png" alt="" />
-                Scale Advanced Annotations
-              </li>
-            </ul>
-            <button>
-              LEARN MORE <img src="img/arrow-down-left.png" alt="" />
-            </button>
-          </div>
-          <div className="tech_img">
-            <img src="img/art4.png" alt="" />
-          </div>
-        </div> */}
       </div>
     </section>
   );
